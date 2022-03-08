c9b38e83b8177f3ce730193a5b2d31fc7eb98adc
Marcus Vinicius <18752038+marcusvinic2@users.noreply.github.com>
2022-03-08 17:10:49 -0300

    importando rotas da api

--- src/Routes/routes.js
+++ src/Routes/routes.js
@@ -1,6 +1,5 @@
 import React from "react";
 import { Switch, Route, BrowserRouter as Router } from "react-router-dom"
-
 import { Login, Home } from '../Components'
 
 function Routes(){
---
+++ src/Services/api.js
@@ -0,0 +1,16 @@
+import axios from 'axios'
+
+//criando conexão com o servidor back-end
+const apiShop = axios.create({
+	baseURL: 'https://fakestoreapi.com'
+})
+
+apiShop.interceptors.request.use( async config => {
+	const userData = await localStorage.getItem('Shop:userData')
+	const token = userData && JSON.parse(userData).token
+	config.headers.authorization = `Bearer ${token}`
+
+	return config
+} )
+
+export default apiShop
